80880ae00ef07bfffdfd07262d7ec09223715b22
adamcagle <adamcagle@gmail.com>
2022-10-21 08:34:20 -0500

    Update and rename todo.R to todo.r

--- todo.R
+++
@@ -1,4 +0,0 @@
-TODO: Test todo action
-hello_world <- function() {
-print("Hello World!")
-}
---
+++ todo.r
@@ -0,0 +1,6 @@
+TODO: Test todo action
+hello_world <- function() {
+print("Hello World!")
+}
+
+TODO: Change .R file extension to .r to test if file extension case sensitivity is the issue
