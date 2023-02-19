77324097eeeac222962e7a2d4cdff08d9375f3e9
adamcagle <adamcagle@gmail.com>
2023-02-19 17:54:47 -0600

    Update todo.R

--- todo.R
+++ todo.R
@@ -1,24 +1,19 @@
 # TODO: Improve support for markdown in comments
 # Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
-# I can link to prior issues such as #1
-# 
+# I can link to prior issues such as #3 and #4
 # I can add a [hyperlink](https://www.google.com)
-# 
 # I can add a list
 #   * Item 1
 #   * Item 2
 #   * Item 3
-#
 # ## Heading Level 2
 # These seem to throw things off
-# 
 # ## Another Heading
 # Another List:
 #   * Item 1
 #   * Item 2
 # And assignees doesn't seem to get picked up.
 # assignees: adamcagle
-
 hello_world <- function() {
 print("Hello World!")
 }
