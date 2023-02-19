2857adf2521399933f2cb1fc010a3bdfef94b54e
adamcagle <adamcagle@gmail.com>
2023-02-19 17:49:12 -0600

    Try using markdown

--- todo.R
+++ todo.R
@@ -1,15 +1,27 @@
-#TODO: Test todo action
+# TODO: Add support for markdown in comments
+# Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
+# 
+# I can link to prior issues such as #1
+# 
+# I can add a [hyperlink](https://www.google.com)
+# 
+# I can add a list
+#   * Item 1
+#   * Item 2
+#   * Item 3
+#
+# ## Heading Level 2
+# These seem to throw things off
+# 
+# ## Another Heading
+# Another List:
+#   * Item 1
+#   * Item 2
+# And assignees doesn't seem to get picked up.
+# asignees: adamcagle
+
 hello_world <- function() {
 print("Hello World!")
 }
 
 #TODO: Change .R file extension to .r to test if file extension case sensitivity is the issue
-
-
-#TODO: New Issue
-
-# TODO: With space
-
-# TODO: Close Issue
-
-# TODO: Should work now!
