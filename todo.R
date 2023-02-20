0e4553ddbe4e126dfde6fe0df83c208c06e3cbf5
adamcagle <adamcagle@gmail.com>
2023-02-20 17:05:31 -0600

    Try without Markdown

--- todo.R
+++ todo.R
@@ -1,22 +1,22 @@
-# TODO: Test single character indentation of subsequent lines
+# TODO: Try Removing Markdown
 #  Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
 # 
-#  I can link to prior issues such as #1
+#  I can link to prior issues such as 1
 # 
-#  I can add a [hyperlink](https://www.google.com)
+#  I can add a hyperlink https://www.google.com
 # 
 #  I can add a list
-#    * Item 1
-#    * Item 2
-#    * Item 3
+#    Item 1
+#    Item 2
+#    Item 3
 #
-#  ## Heading Level 2
+#  Heading Level 2
 #  These seem to throw things off
 # 
-#  ## Another Heading
+#  Another Heading
 #  Another List:
-#    * Item 1
-#    * Item 2
+#    Item 1
+#    Item 2
 #  And assignees doesn't seem to get picked up.
 #  assignees: adamcagle
 hello_world <- function() {
