002a4d48fe6aef00cdaee6b1429b77a77be00619
adamcagle <adamcagle@gmail.com>
2023-02-20 17:14:17 -0600

    Clean Start

---
+++ todo2.R
@@ -0,0 +1,24 @@
+# TODO: Try Removing Markdown And Use New File
+#  Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
+# 
+#  I can link to prior issues such as 1
+# 
+#  I can add a hyperlink https://www.google.com
+# 
+#  I can add a list
+#    Item 1
+#    Item 2
+#    Item 3
+#
+#  Heading Level 2
+#  These seem to throw things off
+# 
+#  Another Heading
+#  Another List:
+#    Item 1
+#    Item 2
+#  And assignees doesn't seem to get picked up.
+#  assignees: adamcagle
+hello_world <- function() {
+print("Hello World!")
+}
