658ce321910135a3490b899de762df960af99612
adamcagle <adamcagle@gmail.com>
2023-02-20 09:35:27 -0600

    Update todo.R

--- todo.R
+++ todo.R
@@ -1,13 +1,18 @@
-# TODO: Improve support for markdown in todo comments
+# TODO: Add support for markdown in comments
 # Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
-# I can link to prior issues such as #3 and #4
+# 
+# I can link to prior issues such as #1
+# 
 # I can add a [hyperlink](https://www.google.com)
+# 
 # I can add a list
 #   * Item 1
 #   * Item 2
 #   * Item 3
+#
 # ## Heading Level 2
 # These seem to throw things off
+# 
 # ## Another Heading
 # Another List:
 #   * Item 1
@@ -17,5 +22,3 @@
 hello_world <- function() {
 print("Hello World!")
 }
-
-#TODO: Change .R file extension to .r to test if file extension case sensitivity is the issue
