4b6687af5eee0e165769148ea9eef3d8a9214e46
adamcagle <adamcagle@gmail.com>
2023-02-20 16:49:17 -0600

    Test indentation

--- todo.R
+++ todo.R
@@ -1,24 +1,24 @@
-# TODO: Add support for markdown in comments
-# Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
+# TODO: Test single character indentation of subsequent lines
+#  Markdown sort of works, but it seems that the body is sometimes truncated prematurely. I'm not sure what causes this.
 # 
-# I can link to prior issues such as #1
+#  I can link to prior issues such as #1
 # 
-# I can add a [hyperlink](https://www.google.com)
+#  I can add a [hyperlink](https://www.google.com)
 # 
-# I can add a list
-#   * Item 1
-#   * Item 2
-#   * Item 3
+#  I can add a list
+#    * Item 1
+#    * Item 2
+#    * Item 3
 #
-# ## Heading Level 2
-# These seem to throw things off
+#  ## Heading Level 2
+#  These seem to throw things off
 # 
-# ## Another Heading
-# Another List:
-#   * Item 1
-#   * Item 2
-# And assignees doesn't seem to get picked up.
-# assignees: adamcagle
+#  ## Another Heading
+#  Another List:
+#    * Item 1
+#    * Item 2
+#  And assignees doesn't seem to get picked up.
+#  assignees: adamcagle
 hello_world <- function() {
 print("Hello World!")
 }
